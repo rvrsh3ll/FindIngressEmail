b38b29751c5fb60c85d6af9771fcb434f3473633
Steve Borosh <rvrsh3ll@users.noreply.github.com>
2023-12-14 17:24:17 -0500

    Refactor With additional options

--- FindIngressEmail.ps1
+++ FindIngressEmail.ps1
@@ -1,4 +1,76 @@
 function Invoke-FindIngressEmail {
+    <#
+        .SYNOPSIS
+
+        Check mail filters from random domains.
+
+        .DESCRIPTION
+
+        A script to test the efficacy of SPAM filters.
+
+        .PARAMETER SMTPServer
+
+        Target SMTP server.
+
+        .PARAMETER Subject
+
+        Specifies the subject of the email.
+
+        .PARAMETER BodyFile
+
+        Specify an HTML formatted file for the email body.
+
+        .PARAMETER FromFile
+
+        Specify a file with a single or multiple From email addresses 'noreply@microsoofty.com'.
+
+        .PARAMETER ToEmail
+
+        Specify the To email address.
+
+        .PARAMETER EmailSmtpUser
+
+        Specify an SMTP user.
+
+        .PARAMETER EmailSmtpPass
+
+        Specify an SMTP password.
+
+        .PARAMETER BodyEncoding
+
+        Specify the Body Encoding.
+
+        .PARAMETER SubjectEncoding
+
+        Specify the Subject Encoding.
+
+        .PARAMETER HeadersEncoding
+
+        Specify the Body Headers Encoding.
+
+        .PARAMETER BodyTransferEncoding
+
+        Specify the Body Transfer Encoding.
+
+        .PARAMETER Delay
+
+        Specify the delay between sending emails.
+
+        .PARAMETER RetryDelay
+
+        Specify the delay between rate limited emails.
+
+
+        .EXAMPLE
+
+        C:\PS> Import-Module .\FindIngressEmail.ps1
+        # Example using a proxy SMTP service such as mailgun.
+        C:\PS> Invoke-FindIngressEmail -SMTPServer smtp.mailgun.org -Subject "Your Device Code Expired" -BodyFile ./DeviceCode.html -FromFile ./fromEmails.txt -EmailSmtpUser "postmaster@<insert domain>" -EmailSmtpPass "SMTPPass" -Delay 5 -RetryDelay 35 -Verbose -ToEmail "target@<insert domain>"
+        # Example using a Microsoft Direct Send endpoint to spoof internally.
+        C:\PS> Invoke-FindIngressEmail -SMTPServer <insertcompany>.mail.protection.outlook.com -Subject "Your Device Code Expired" -BodyFile ./DeviceCode.html -FromFile ./fromEmails.txt -Delay 5 -RetryDelay 35 -Verbose -ToEmail "target@<insert domain>"
+        # Example using different encoding options.
+        C:\PS> Invoke-FindIngressEmail -SMTPServer <insertcompany>.mail.protection.outlook.com -Subject "Your Device Code Expired" -BodyFile ./DeviceCode.html -FromFile ./fromEmails.txt -Delay 5 -RetryDelay 35 -Verbose -ToEmail "target@<insert domain>" -BodyEncoding bigendianunicode -SubjectEncoding oem -HeadersEncoding utf8NoBOM -BodyTransferEncoding EightBit
+        #>
 
     [CmdletBinding()]
     param(
@@ -13,8 +85,21 @@ function Invoke-FindIngressEmail {
     [Parameter(Mandatory=$true)]
     [String]$ToEmail,
     [Parameter(Mandatory=$false)]
+    [String]$EmailSmtpUser,
+    [Parameter(Mandatory=$false)]
+    [String]$EmailSmtpPass,
+    [Parameter(Mandatory=$false)]
     [ValidateSet("ascii","bigendianutf32","unicode","utf8","utf8NoBOM","bigendianunicode","oem","utf7","utf8BOM","utf32")]
-    [String]$Encoding = "ascii",
+    [String]$BodyEncoding = "ascii",
+    [Parameter(Mandatory=$false)]
+    [ValidateSet("ascii","bigendianutf32","unicode","utf8","utf8NoBOM","bigendianunicode","oem","utf7","utf8BOM","utf32")]
+    [String]$SubjectEncoding = "ascii",
+    [Parameter(Mandatory=$false)]
+    [ValidateSet("ascii","bigendianutf32","unicode","utf8","utf8NoBOM","bigendianunicode","oem","utf7","utf8BOM","utf32")]
+    [String]$HeadersEncoding = "ascii",
+    [Parameter(Mandatory=$false)]
+    [ValidateSet("QuotedPrintable","Base64","SevenBit","EightBit","Unknown")]
+    [String]$BodyTransferEncoding = "SevenBit",
     [Parameter(Mandatory=$false)]
     [Int]$Delay = 10,
     [Parameter(Mandatory=$false)]
@@ -22,14 +107,30 @@ function Invoke-FindIngressEmail {
     )
     $EmailBody = Get-Content $BodyFile |Out-String
     $FromEmails = Get-Content $FromFile
-    $WarningPreference='silentlycontinue'
-    
-    # Default rate limit is 30 messages per minute https://learn.microsoft.com/en-us/office365/servicedescriptions/exchange-online-service-description/exchange-online-limits?redirectedfrom=MSDN#RecipientLimits
-
-    $fromEmails|%{
+    $m = New-Object System.Net.Mail.MailMessage
+    $m.IsBodyHtml = $true
+    $m.to.Add($ToEmail)
+    $m.Body = $EmailBody
+    $m.subject = $Subject
+    $fromEmails| % {
         try {
-            Send-MailMessage -SmtpServer $SmtpServer -Subject $Subject -Body $EmailBody -BodyAsHtml -From $_ -To $ToEmail -UseSSL -Encoding $Encoding -ErrorAction Stop
-            Write-Verbose -Message "[*] Sending from $_ to $ToEmail .."
+            $FromAddress = $_
+            $m.From = $FromAddress
+            $m.Sender = $FromAddress
+            $m.ReplyToList.Add($FromAddress)
+            $m.BodyEncoding = [System.Text.Encoding]::$BodyEncoding
+            $m.SubjectEncoding = [System.Text.Encoding]::$SubjectEncoding
+            $m.HeadersEncoding = [System.Text.Encoding]::$HeadersEncoding
+            $m.BodyTransferEncoding = [System.Net.Mime.TransferEncoding]::$BodyTransferEncoding
+            $m.Headers.Add('Return-Path',$FromAddress)
+            $smtp = new-object Net.Mail.SmtpClient($SMTPServer)
+            if ($EmailSmtpUser -and $EmailSmtpPass) {
+                $smtp.Credentials =  New-Object System.Net.NetworkCredential($EmailSmtpUser , $EmailSmtpPass)
+            } else {
+                Write-Output -Message "[*] Both Username and Password are required."
+            }
+            $smtp.Send($m)
+            Write-Verbose -Message "[*] Sending from $FromAddress to $ToEmail .."
             Start-Sleep -Seconds $Delay
         }
         catch {
--- README.md
+++ README.md
@@ -6,11 +6,15 @@
 ### Quick Send
 
 
- Invoke-FindIngressEmail -SMTPServer example.mail.protection.outlook.com -Subject "Device Code" -BodyFile ./email.html -FromFile fromEmails.txt -ToEmail "ceo@example.com" -Encoding unicode -Delay 15 -RetryDelay 35 -Verbose |tee outlog.txt
+ Invoke-FindIngressEmail -SMTPServer example.mail.protection.outlook.com -Subject "Device Code" -BodyFile ./email.html -FromFile ./fromEmails.txt -ToEmail "ceo@example.com" -Encoding unicode -Delay 15 -RetryDelay 35 -Verbose |tee outlog.txt
 
+
+### Using a Mail Proxy with SMTP Authentication
+
+Start-Job -name asciiDeviceCode -ScriptBlock {  Import-Module /home/rev/FindIngressEmails.ps1; Invoke-FindIngressEmail -SMTPServer example.mail.protection.outlook.com -Subject "Your Device Code Expired" -BodyFile ./DeviceCode.html -FromFile ./fromEmails.txt -Delay 5 -RetryDelay 35 -Verbose -ToEmail "target@example.com" -EmailSmtpUser loginUser -EmailSmtpPass loginPasword }
 
 ### Azure Cloud Shell Job
 
 
-Start-Job -name asciiDeviceCode -ScriptBlock {  Import-Module /home/rev/FindIngressEmails.ps1; Invoke-FindIngressEmail -smtpServer example.mail.protection.outlook.com -Subject "Microsoft 365 Session Sync Required" -bodyFile /root/dev/email.html -fromFile -toEmail $_ -Delay 10 -Encoding ascii }
+Start-Job -name asciiDeviceCode -ScriptBlock {  Import-Module /home/rev/FindIngressEmails.ps1; Invoke-FindIngressEmail -SMTPServer example.mail.protection.outlook.com -Subject "Your Device Code Expired" -BodyFile ./DeviceCode.html -FromFile ./fromEmails.txt -Delay 5 -RetryDelay 35 -Verbose -ToEmail "target@example.com" -BodyEncoding bigendianunicode -SubjectEncoding oem -HeadersEncoding utf8NoBOM -BodyTransferEncoding EightBit }
 
